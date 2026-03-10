6ef5e28446aa2a30f70a08f2119d664c49a556b6
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-03-10 18:09:26 +0100

    feat(admin): Implement  django admin to student and course

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from school.models.courses import Course
+from school.models.students import Student
+
+
+class Students(admin.ModelAdmin):
+    list_display = ("id", "name", "email", "cpf", "date_of_birth", "phone_number")
+    list_display_links = (
+        "id",
+        "name",
+    )
+    list_per_page = 20
+    search_fields = ("name",)
+
+
+admin.site.register(Student, Students)
+
+
+class Courses(admin.ModelAdmin):
+    list_display = ("id", "code", "description")
+    list_display_links = ("id", "code")
+    search_fields = ("code",)
+
+
+admin.site.register(Course, Courses)
