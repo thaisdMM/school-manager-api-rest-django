456323605e4fbe7438d5c9ca377b8c6fc616e5de
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-03-12 16:17:45 +0100

    feat(urls): Implement students and courses rest_framework default routers

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
-from school.views import students
+from django.urls import path, include
+from rest_framework import routers
 
-urlpatterns = [path("admin/", admin.site.urls),
-                path("students", students)]
+from school.views import StudentViewSet, CourseViewSet
+
+router = routers.DefaultRouter()
+router.register(prefix="students", viewset=StudentViewSet, basename="Students")
+router.register(prefix="courses", viewset=CourseViewSet, basename="Courses")
+
+urlpatterns = [path("admin/", admin.site.urls), path("", include(router.urls))]
