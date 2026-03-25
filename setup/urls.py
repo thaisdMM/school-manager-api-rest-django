7d22f10a8e4b3fe155b83181053bc880f498e110
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-03-25 17:56:34 +0100

    feat(urls): add enrollment router

--- setup/urls.py
+++ setup/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from school.views import StudentViewSet, CourseViewSet
+from school.views import StudentViewSet, CourseViewSet, EnrollmentViewSet
 
 router = routers.DefaultRouter()
 router.register(prefix="students", viewset=StudentViewSet, basename="Students")
 router.register(prefix="courses", viewset=CourseViewSet, basename="Courses")
+router.register(prefix="enrollment", viewset=EnrollmentViewSet, basename="Enrollment")
 
 urlpatterns = [path("admin/", admin.site.urls), path("", include(router.urls))]
