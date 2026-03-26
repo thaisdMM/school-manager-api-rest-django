99998a2c641e09aa14d78f4f9cb0a0905c37f17a
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-03-26 17:24:41 +0100

    feat: implement rest framework default authentication and permission classes

--- school/views.py
+++ school/views.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 from rest_framework import viewsets, generics
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+
 
 from school.models import Student
 from school.models import Course
@@ -16,30 +15,21 @@ from school.serializers import (
 
 
 class StudentViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
 
 class EnrollmentViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-
     queryset = Enrollment.objects.all()
     serializer_class = EnrollmentSerializer
 
 
 class ListEnrollmentsStudentView(generics.ListAPIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = Enrollment.objects.filter(student_id=self.kwargs["pk"])
@@ -49,8 +39,6 @@ class ListEnrollmentsStudentView(generics.ListAPIView):
 
 
 class ListEnrollmentsCourseView(generics.ListAPIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = Enrollment.objects.filter(course_id=self.kwargs["pk"])
--- setup/settings.py
+++ setup/settings.py
@@ -111,3 +111,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
