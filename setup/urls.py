df9b1a86e6eaf2c11fd5cbcd468eee04ff5e5ea6
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-03-26 15:47:34 +0100

    feat(urls): update urlpatterns to add especific path to ListEnrollmentsStudentView and ListEnrollmentsCourseView; fix router prefix to enrollments

--- setup/urls.py
+++ setup/urls.py
@@ -2,11 +2,22 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from school.views import StudentViewSet, CourseViewSet, EnrollmentViewSet
+from school.views import (
+    StudentViewSet,
+    CourseViewSet,
+    EnrollmentViewSet,
+    ListEnrollmentsStudentView,
+    ListEnrollmentsCourseView,
+)
 
 router = routers.DefaultRouter()
 router.register(prefix="students", viewset=StudentViewSet, basename="Students")
 router.register(prefix="courses", viewset=CourseViewSet, basename="Courses")
-router.register(prefix="enrollment", viewset=EnrollmentViewSet, basename="Enrollment")
+router.register(prefix="enrollments", viewset=EnrollmentViewSet, basename="Enrollment")
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include(router.urls))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
+    path("students/<int:pk>/enrollments", ListEnrollmentsStudentView.as_view()),
+    path("courses/<int:pk>/enrollments", ListEnrollmentsCourseView.as_view()),
+]
