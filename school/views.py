a52273a69be0a2b451e0257bd4488612d69a306c
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-03-26 15:45:15 +0100

    feat(views): add ListEnrollmentsStudentView and ListEnrollmentsCourseView using generics.ListAPIView

--- school/views.py
+++ school/views.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
 from school.models import Student
 from school.models import Course
 from school.models import Enrollment
-from school.serializers import StudentSerializer, CourseSerializer, EnrollmentSerializer
+from school.serializers import (
+    StudentSerializer,
+    CourseSerializer,
+    EnrollmentSerializer,
+    ListEnrollmentsCourseSerializer,
+    ListEnrollmentsStudentSerializer,
+)
 
 
 class StudentViewSet(viewsets.ModelViewSet):
@@ -20,3 +26,19 @@ class CourseViewSet(viewsets.ModelViewSet):
 class EnrollmentViewSet(viewsets.ModelViewSet):
     queryset = Enrollment.objects.all()
     serializer_class = EnrollmentSerializer
+
+
+class ListEnrollmentsStudentView(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Enrollment.objects.filter(student_id=self.kwargs["pk"])
+        return queryset
+
+    serializer_class = ListEnrollmentsStudentSerializer
+
+
+class ListEnrollmentsCourseView(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Enrollment.objects.filter(course_id=self.kwargs["pk"])
+        return queryset
+
+    serializer_class = ListEnrollmentsCourseSerializer
