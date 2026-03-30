09a6b7469d33eac90d2fbf8fa0568d191196f987
Thaís Moreira <thaisdmedeiros@gmail.com>
2026-03-30 13:44:37 +0200

    feat(models): update course model to add unique and MinLengthValidator to code and apply migrations

--- school/models/courses.py
+++ school/models/courses.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 class Course(models.Model):
     LEVEL = (("B", "Basic"), ("I", "Intermediate"), ("A", "Advanced"))
-    code = models.CharField(max_length=10)
+    code = models.CharField(
+        max_length=10, unique=True, validators=[MinLengthValidator(3)]
+    )
     description = models.CharField(max_length=100, blank=False)
     level = models.CharField(
         max_length=1, choices=LEVEL, blank=False, null=False, default="B"
